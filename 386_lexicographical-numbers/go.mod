85cca7d64ad0774625a40c31f107137410fb5992
luocaiyi <luocaiyimissmei@outlook.com>
2022-04-18 11:15:16 +0800

    add 386 code

---
+++ 386_lexicographical-numbers/go.mod
@@ -0,0 +1,3 @@
+module 386_lexicographical-numbers
+
+go 1.17
---
+++ 386_lexicographical-numbers/main_test.go
@@ -0,0 +1,10 @@
+package _86_lexicographical_numbers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLexicalOrder(t *testing.T) {
+	fmt.Printf("%+v\n", lexicalOrder(130))
+}
